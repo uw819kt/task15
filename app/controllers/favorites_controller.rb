f444a0352545eea972610cb2864085eb52fc6e5c
uw819kt <miya0arima@gmail.com>
2024-12-23 21:07:03 +0900

    first_commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,20 @@
 class FavoritesController < ApplicationController
 
   def create
-    current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
+    binding.irb
+    @favorites = current_user.favorites.create(blog_id: params[:blog_id])
+    respond_to do |format|
+      format.js { render :index }
+    # redirect_to blogs_path
+    end
   end
 
   def destroy
     current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    respond_to do |format|
+      format.js { render :index }
+    # redirect_to blogs_path
+    end
   end
 
 end
