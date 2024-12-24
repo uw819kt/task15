29def64319532641a5bde480334f1d6a682fd4b3
uw819kt <miya0arima@gmail.com>
2024-12-24 09:42:48 +0900

    orrection_2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,17 @@
 class FavoritesController < ApplicationController
 
   def create
-    @favorites = current_user.favorites.create(blog_id: params[:blog_id])
+    current_user.favorites.create(blog_id: params[:blog_id])
     respond_to do |format|
-      format.js { render 'index', locals: { favorites: @favorites } }
+      format.js { render :index }
     # redirect_to blogs_path
-    binding.irb
     end
   end
 
   def destroy
-    @favorites = current_user.favorites.find_by(id: params[:id])
-    @favorites.destroy if @favorites
+    current_user.favorites.find_by(id: params[:id]).destroy
     respond_to do |format|
-      format.js { render 'index', locals: { favorites: @favorites } }
+      format.js { render :index }
     # redirect_to blogs_path
     end
   end
